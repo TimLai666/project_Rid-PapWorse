39f72467e3276aa45a658d5085b2e437220cb9a8
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-04-02 22:37:11 +0800

    更新

--- SwiftDocz/launcher.py
+++ SwiftDocz/launcher.py
@@ -1,8 +1,19 @@
 import eel
+import time
+
 eel.init('UI')  # 假设您的HTML/CSS/JavaScript文件都放在`web`目录下
 
-@eel.expose  # 暴露Python函数给JavaScript
-def greet_python(name):
-    return f"Hello, {name}!"
+def switchPage(page: str):
+    eel.switchPage(page)
+
+@eel.expose
+def page_ready():
+    # 程式ui打開後會立即執行
+    pass
+    
+
+# switchPage('home')
 
 eel.start('main.html', size=(1920, 1080), port=5349)  # 启动应用，打开`main.html`
+
+
