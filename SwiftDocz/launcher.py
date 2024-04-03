74cc9dc9757ca3d454b56e94e1ded4127a0d0497
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-04-03 17:02:59 +0800

    更新

--- SwiftDocz/launcher.py
+++ SwiftDocz/launcher.py
@@ -11,9 +11,12 @@ def page_ready():
     # 程式ui打開後會立即執行
     pass
     
-
 # switchPage('home')
 
 eel.start('main.html', size=(1920, 1080), port=5349)  # 启动应用，打开`main.html`
 
 
+# mac 要使用 python 3.10
+
+# 打包命令
+# pyinstaller --add-data 'UI:UI' launcher.py
