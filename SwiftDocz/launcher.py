7f91807db73bbec1e3c7e3aca75ec1d2491fa5d0
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-04-05 19:22:28 +0800

    改用paling

--- SwiftDocz/launcher.py
+++ SwiftDocz/launcher.py
@@ -1,23 +1,24 @@
-import eel
+import paling
 import time
 
-eel.init('UI')  # 假设您的HTML/CSS/JavaScript文件都放在`web`目录下
+paling.init('UI')  # 假设您的HTML/CSS/JavaScript文件都放在`web`目录下
 
-@eel.expose
+@paling.expose
 def switchPage(page: str):
-    eel.switchPage(page)
+    paling.switchPage(page)
 
-@eel.expose
+@paling.expose
 def page_ready():
     # 程式ui打開後會立即執行
     pass
     
 # switchPage('home')
 
-eel.start('main.html', size=(1910, 1080), port=5349)  # 启动应用，打开`main.html`
+paling.start('main.html', size=(1910, 1080), port=5349)  # 启动应用，打开`main.html`
 
 
 # mac 要使用 python 3.10
+# windows 要使用 python 3.11
 
 # 打包命令
 # pyinstaller --add-data 'UI:UI' launcher.py
